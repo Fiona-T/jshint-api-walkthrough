83ab99c8a71a6daaa9eb7d17a6057d7a89e0f2ad
Fiona-T <fitracey@gmail.com>
2021-09-13 11:46:58 +0000

    feat: add postForm function to POST form details to API

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,41 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 // using Bootstrap Modal function 
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
-// the button Check Key, that opens the modal 
+// the button Check Key, that opens the modal - and also does the GET request to the API
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+// the button Run Checks, that sends the data to be checked - POST request
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+async function postForm(e) {
+    const form = new FormData(document.getElementById("checksform"));
+
+    // a test to ensure form captured the data. Log to console using entries method
+    // for (let entry of form.entries()) {
+    //     console.log(entry);
+    // }
+
+    // make POST request to the API, authorise it with the API_KEY
+    // attach the form data as the body of the request 
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        },
+        body: form,
+    });
+
+    // convert response to json
+    const data = await response.json();
+
+    // if ok is true then log to console, otherwise throw Error
+    if(response.ok) {
+        console.log(data);
+    } else {
+        throw new Error(data.error);
+    }
+
+}
 
 // GET request to API_URL with API_KEY, pass the results to function that will display them
 // asynchronous function - instead of using .then, wrap the function in async function 
@@ -33,6 +66,6 @@ async function getStatus(e) {
 function displayStatus(data) {
     document.getElementById("resultsModalTitle").textContent = "API Key Status";
     document.getElementById("results-content").textContent = `Your key is valid until ${data.expiry}`
-
+    //Bootstrap modal function
     resultsModal.show();
 }
